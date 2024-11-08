8fc71beedcc50a718b7d2a39e8e7072ee07e81e5
William Killerud <william@killerud.com>
2024-11-08 21:17:07 +0100

    Test selector symbols end to end

--- pkgs/sass_language_services/lib/src/configuration/language_configuration.dart
+++ pkgs/sass_language_services/lib/src/configuration/language_configuration.dart
@@ -4,22 +4,22 @@ class FeatureConfiguration {
   FeatureConfiguration({required this.enabled});
 }
 
-class DefinitionConfiguration extends FeatureConfiguration {
-  DefinitionConfiguration({required super.enabled});
+class DocumentSymbolsConfiguration extends FeatureConfiguration {
+  DocumentSymbolsConfiguration({required super.enabled});
 }
 
-class LinksConfiguration extends FeatureConfiguration {
-  LinksConfiguration({required super.enabled});
+class DocumentLinksConfiguration extends FeatureConfiguration {
+  DocumentLinksConfiguration({required super.enabled});
 }
 
 class LanguageConfiguration {
-  late final DefinitionConfiguration definition;
-  late final LinksConfiguration links;
+  late final DocumentSymbolsConfiguration documentSymbols;
+  late final DocumentLinksConfiguration documentLinks;
 
   LanguageConfiguration.from(dynamic config) {
-    definition = DefinitionConfiguration(
-        enabled: config?['definition']?['enabled'] as bool? ?? true);
-    links = LinksConfiguration(
-        enabled: config?['links']?['enabled'] as bool? ?? true);
+    documentSymbols = DocumentSymbolsConfiguration(
+        enabled: config?['documentSymbols']?['enabled'] as bool? ?? true);
+    documentLinks = DocumentLinksConfiguration(
+        enabled: config?['documentLinks']?['enabled'] as bool? ?? true);
   }
 }
