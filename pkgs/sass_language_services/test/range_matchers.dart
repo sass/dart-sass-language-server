0b98015e9d5ce6eae3c59a07a99266476e455d1b
William Killerud <william@killerud.com>
2024-11-10 16:28:18 +0100

    Refactor to match test style

--- pkgs/sass_language_services/test/range_matchers.dart
+++ pkgs/sass_language_services/test/range_matchers.dart
@@ -1,20 +1,6 @@
 import 'package:lsp_server/lsp_server.dart';
 import 'package:test/test.dart';
 
-class AtLine extends CustomMatcher {
-  AtLine(Object? valueOrMatcher)
-      : super('Position at line', 'line', valueOrMatcher);
-
-  featureValueOf(actual) => (actual as Position).line;
-}
-
-class AtCharacter extends CustomMatcher {
-  AtCharacter(Object? valueOrMatcher)
-      : super('Position at character', 'character', valueOrMatcher);
-
-  featureValueOf(actual) => (actual as Position).character;
-}
-
 class StartsAtLine extends CustomMatcher {
   StartsAtLine(Object? valueOrMatcher)
       : super('Range starts at line', 'line', valueOrMatcher);
