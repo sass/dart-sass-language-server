fb4c94d572f164fe85d9008b1de753b3ccb877ee
William Killerud <william@killerud.com>
2024-11-12 20:48:30 +0100

    Go to definition

--- pkgs/sass_language_services/lib/src/features/go_to_definition/go_to_definition_visitor.dart
+++
@@ -1,5 +0,0 @@
-import 'package:sass_api/sass_api.dart' as sass;
-
-import '../document_symbols/document_symbols_visitor.dart';
-
-class GoToDefinitionVisitor extends DocumentSymbolsVisitor {}
--- pkgs/sass_language_services/lib/src/features/go_to_definition/symbols.dart
+++
@@ -1,6 +0,0 @@
-import 'scope.dart';
-import 'scope_visitor.dart';
-
-class Symbols {
-  final _globalScope = Scope(length: double.maxFinite.floor(), offset: 0);
-}
