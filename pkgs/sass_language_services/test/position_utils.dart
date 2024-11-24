894b70a8b8db4617d8dd117ace6cb269388ceb52
William Killerud <william@killerud.com>
2024-11-24 21:50:56 +0100

    Tests for TextDocument

--- pkgs/sass_language_services/test/position_utils.dart
+++ pkgs/sass_language_services/test/position_utils.dart
@@ -3,3 +3,14 @@ import 'package:lsp_server/lsp_server.dart' as lsp;
 lsp.Position at({required int line, required int char}) {
   return lsp.Position(character: char, line: line);
 }
+
+lsp.Position position(int line, int char) {
+  return at(line: line, char: char);
+}
+
+lsp.Range range(int startLine, int startChar, int endLine, int endChar) {
+  return lsp.Range(
+    start: at(line: startLine, char: startChar),
+    end: at(line: startLine, char: startChar),
+  );
+}
